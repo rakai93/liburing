072cd568414001a193aba43a23665df1ee018a87
Christian Füller <christian.fueller@gmx.de>
2025-04-21 17:26:41 +0200

    Define _GNU_SOURCE in wrapper.h

--- wrapper.h
+++ wrapper.h
@@ -1,4 +1,7 @@
 // Defining IOURINGINLINE upfront prevents liburing.h to define it as "static inline"
 // and makes all functions visible to bindgen.
 #define IOURINGINLINE
+
+#define _GNU_SOURCE
+
 #include "lib/src/include/liburing.h"
